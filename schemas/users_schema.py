6179f7974b2b0507f2d0ae9fb6a374ba8be07066
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 16:03:35 +0300

    Created the users schemas

--- schemas/users_schema.py
+++ schemas/users_schema.py
@@ -1,1 +1,23 @@
 """The schema file for the users"""
+from typing import List
+
+from pydantic import BaseModel, EmailStr
+
+from schemas.article_schema import Article
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    
+
+class UserCreate(UserBase):
+    password: str
+    
+    
+class User(UserBase):
+    id: int
+    is_active: bool
+    articles: List[Article] = []
+    
+    class Config:
+        orm_mode = True
