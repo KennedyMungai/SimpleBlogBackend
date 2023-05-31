f0a11b28e560a30ff9a0c5f72d5a8c4e23393d26
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 15:56:38 +0300

    Reorganized the imports for the file

--- schemas/article_schema.py
+++ schemas/article_schema.py
@@ -1,7 +1,8 @@
 """The schema file for the articles"""
 import email
-from typing import Optional, List
 from datetime import datetime
+from typing import List, Optional
+
 from pydantic import BaseModel, EmailStr
 
 
