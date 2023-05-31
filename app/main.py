ccbb4d9f9dc6a5f4bc5d158a2c222cc5a7abe1d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 15:12:02 +0300

    Basic boiler plate for the app

---
+++ app/main.py
@@ -0,0 +1,11 @@
+"""The entrypoint for the application script"""
+from fastapi import FastAPI
+
+
+app = FastAPI(name="SImple Blog Project", description="A simple blog project", version="0.1.0")
+
+
+@app.get("/", name='Root', description='The root endpoint for the application')
+async def root():
+    """The root endpoint for the application"""
+    return {"Message": "App Works"}
