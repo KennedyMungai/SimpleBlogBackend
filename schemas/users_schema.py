1dca7dab404f5892992c206447ad00bc9f95bae1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 16:04:48 +0300

    Added soctrings to the user schemas

--- schemas/users_schema.py
+++ schemas/users_schema.py
@@ -7,14 +7,29 @@ from schemas.article_schema import Article
 
 
 class UserBase(BaseModel):
+    """The base schema for the user data
+
+    Args:
+        BaseModel (Pydantic): The base class for the schema data
+    """
     email: EmailStr
     
 
 class UserCreate(UserBase):
+    """The schema for the user creation
+
+    Args:
+        UserBase (UserBase): The base schema for the user data
+    """
     password: str
     
     
 class User(UserBase):
+    """The template of the user data in the database
+
+    Args:
+        UserBase (Pydantic): The base for the schemas
+    """
     id: int
     is_active: bool
     articles: List[Article] = []
