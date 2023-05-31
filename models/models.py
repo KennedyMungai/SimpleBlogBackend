aa35e0e0ab180aff01755208a6e94e79af92bf46
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 16:10:18 +0300

    Added and reorganized imports for the file

--- models/models.py
+++ models/models.py
@@ -1,1 +1,7 @@
 """The file to hold the models for the app"""
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from database.db import Base
