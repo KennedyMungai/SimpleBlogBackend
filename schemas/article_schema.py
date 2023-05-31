21265ca5f6374908b84a1417d55346ab81e6fce5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 15:56:17 +0300

    Added the schemas for the article

--- schemas/article_schema.py
+++ schemas/article_schema.py
@@ -1,1 +1,32 @@
 """The schema file for the articles"""
+import email
+from typing import Optional, List
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+
+class ArticleBase(BaseModel):
+    title: str
+    body: Optional[str] = None
+    
+
+class ArticleCreate(ArticleBase):
+    pass
+
+
+class Article(ArticleBase):
+    id: int
+    author: EmailStr
+    created_at: datetime
+    updated_at: Optional[datetime]
+    
+    class Config:
+        orm_mode = True
+        
+    
+class ArticleUpdate(BaseModel):
+    title: Optional[str]
+    body: Optional[str]
+    
+    class Config:
+        orm_mode = True
