8223c37262db970aff5a4fe63e386e244b280453
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 15:17:34 +0300

    Added the necessary imports

--- database/db.py
+++ database/db.py
@@ -1,1 +1,6 @@
 """The database connection file for the application"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv, find_dotenv
+import os
