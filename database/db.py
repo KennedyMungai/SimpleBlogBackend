704f34bef50dccac0415eacbf0e4f41582b46616
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 15:25:21 +0300

    Some more db setup code

--- database/db.py
+++ database/db.py
@@ -14,3 +14,9 @@ MYSQL_DB = os.environ.get("MYSQL_DB")
 MYSQL_PORT = os.environ.get("MYSQL_PORT")
 
 MYSQL_URL = f'mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
+
+engine = create_engine(MYSQL_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
