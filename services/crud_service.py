467d9b63a256479b07f20946439513efde5ba231
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-02 09:49:01 +0300

    Created a function to get the current user and reorganized the imports

--- services/crud_service.py
+++ services/crud_service.py
@@ -5,9 +5,13 @@ from fastapi import HTTPException
 from sqlalchemy import update
 from sqlalchemy.orm import Session
 
-from models import *
+from models.models import Article, User
 from schemas.article_schema import *
 from schemas.users_schema import *
 
-
 hash = hashlib.sha256()
+
+
+def get_user(_db: Session, user_id: int):
+    """Get user by id"""
+    return _db.query(User).filter(User.id == user_id).first()
