b917cc87df38dae38e16cb37bf6daa5885e9031f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-02 10:30:12 +0300

    Created the endpoint to create users

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import Session
 
 from database.db import engine, get_db
 from schemas.article_schema import Article
-from schemas.users_schema import User
+from schemas.users_schema import User, UserCreate
+from services.crud_service import create_user, get_user_by_email
 
 app = FastAPI(name="SImple Blog Project", description="A simple blog project", version="0.1.0")
 
@@ -15,3 +16,25 @@ app = FastAPI(name="SImple Blog Project", description="A simple blog project", v
 async def root():
     """The root endpoint for the application"""
     return {"Message": "App Works"}
+
+
+@app.post("/users", name='Create User', description='Create a new user', response_model=User)
+async def create_user(_user: UserCreate, _db: Session = Depends(get_db)):
+    """An endpoint to create the usr in the
+
+    Args:
+        _user (UserCreate): The schema used to create users in the database
+        _db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Raises:
+        HTTPException: A 400 is raised if the user already exists
+
+    Returns:
+        User: The user created
+    """
+    _db_user = get_user_by_email(_user.email, _db)
+    if _db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+    
+    return create_user(_user, _db)
+    
